65635365048058ee823f85329432dbc7d12be2a5
plibogor122 <plibogor116@gmail.com>
2024-08-12 10:33:20 +0700

    config

---
+++ main.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCxlxuwJuUjYA4khjmWO7qV9xtfww8pGjE",
+  authDomain: "insan-cemerlang-d0a62.firebaseapp.com",
+  projectId: "insan-cemerlang-d0a62",
+  storageBucket: "insan-cemerlang-d0a62.appspot.com",
+  messagingSenderId: "494535112853",
+  appId: "1:494535112853:web:6043f080f2fd288ec4c39c",
+  measurementId: "G-ZC99LK7VK0"
+};
